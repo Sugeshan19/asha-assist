63814fe32da63db3312c72b68520158937f8764a
Sugeshan <sugeshans62@gmail.com>
2026-03-20 17:46:29 +0530

    Updates

---
+++ assets/Assessment-DWpVsYTG.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-9QSntYGW.js";import{c as t}from"./Typography-BZCKebMj.js";const e=t(s.jsx("path",{d:"M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2M9 17H7v-7h2zm4 0h-2V7h2zm4 0h-2v-4h2z"}),"Assessment");export{e as A};
