b173319587f5a970e262143dde3723e869bbd02b
Sugeshan <sugeshans62@gmail.com>
2026-03-20 17:37:22 +0530

    Updates

---
+++ assets/Warning--8Fii_jM.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Bp2fUewe.js";import{c as o}from"./Typography-Bvy2PM85.js";const m=o(r.jsx("path",{d:"M1 21h22L12 2zm12-3h-2v-2h2zm0-4h-2v-4h2z"}),"Warning");export{m as W};
